5b59baed241a4829239a172b0c3c24c1c375bdb1
Gabriel <gabspereira178@gmail.com>
2025-03-02 16:52:33 +0000

    - update excetpion message to be more comprehensive - repo new searches

--- rentacar-api/src/main/java/com/gabriel/rentacar/exception/accountException/AccountPhoneNumberAlreadyExistsException.java
+++ rentacar-api/src/main/java/com/gabriel/rentacar/exception/accountException/AccountPhoneNumberAlreadyExistsException.java
@@ -5,6 +5,6 @@ import com.gabriel.rentacar.exception.ValidationException;
 public class AccountPhoneNumberAlreadyExistsException extends ValidationException {
   public AccountPhoneNumberAlreadyExistsException(String phoneNumber) {
     super(String.format("Phone number already exists: %s", phoneNumber),
-            "This phone number is already registered with another account");
+            "This phone number is already registered");
   }
 }
